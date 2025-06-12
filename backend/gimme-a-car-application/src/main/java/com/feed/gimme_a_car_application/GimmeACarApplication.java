bc900d71747ad8b1f2a8da6daee8543ea4822b7d
Marvin-Silva <dejesusdasilva.marvin>
2025-06-12 21:09:08 +0200

    This is the first commit of the project

---
+++ backend/gimme-a-car-application/src/main/java/com/feed/gimme_a_car_application/GimmeACarApplication.java
@@ -0,0 +1,13 @@
+package com.feed.gimme_a_car_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimmeACarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimmeACarApplication.class, args);
+	}
+
+}
---
+++ backend/gimme-a-car-application/src/test/java/com/feed/gimme_a_car_application/GimmeACarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feed.gimme_a_car_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimmeACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
