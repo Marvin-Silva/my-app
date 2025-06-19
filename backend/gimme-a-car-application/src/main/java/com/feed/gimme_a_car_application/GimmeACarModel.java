acdddf7da5294e96ef52ff2cbc7c5460586fb7c2
Marvin-Silva <dejesusdasilva.marvin>
2025-06-19 09:20:03 +0200

    :heavy_plus_sign: Imports JPA dependencies into Object data structure

--- backend/gimme-a-car-application/src/main/java/com/feed/gimme_a_car_application/GimmeACarModel.java
+++ backend/gimme-a-car-application/src/main/java/com/feed/gimme_a_car_application/GimmeACarModel.java
@@ -1,5 +1,15 @@
 package com.feed.gimme_a_car_application;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class GimmeACarModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String model;
+    private Integer year;
 }
